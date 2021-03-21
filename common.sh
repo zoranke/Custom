346ed614f953b938c97175e1d48d39cb58cda753
281677160 <66651997+281677160@users.noreply.github.com>
2021-03-21 12:13:25 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -11,7 +11,7 @@ LIZZZ="package/default-settings/files/zzz-default-settings"
 # 全脚本源码通用diy.sh文件
 Diy_all() {
 DIY_GET_COMMON_SH
-echo "3月13号中午有更新（请看公告）"
+echo "3月21号有新通知（请看公告）"
 git clone -b $REPO_BRANCH --single-branch https://github.com/281677160/openwrt-package package/danshui
 mv "${PATH1}"/AutoBuild_Tools.sh package/base-files/files/bin
 chmod +x package/base-files/files/bin/AutoBuild_Tools.sh
@@ -332,6 +332,7 @@ devices=("phicomm-n1" "rk3328" "s9xxx" "vplus")
 Diy_notice() {
 echo ""
 echo "	《公告内容》"
+echo " 3月21号新通知： luci-app-autopoweroff 改名成 luci-app-autotimeset 增加定时重启网络,有用这插件的注意了"
 echo " 3月13号中午12点正更新：重新排版了信息显示，修复了一小BUG，请大家复制最新的build-openwrt.yml文件内容粘贴到你的build-openwrt.yml上"
 echo " 3月11号早上11点增加了所选插件信息,如果使用的是3月9号最新版有需要的请复制最新的build-openwrt.yml文件内容粘贴到你的build-openwrt.yml上"
 echo " 请大家使用3月9号最新版仓库编译"
